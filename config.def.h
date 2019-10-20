35b35db6b3b1b105993b0f6e5fab0e510b699575
z3bra <contact@z3bra.org>
2019-10-20 17:37:19 +0200

    Modify config.def.h to include titlebars

--- config.def.h
+++ config.def.h
@@ -1,13 +1,10 @@
-/* modifier key used for wm actions */
-#define MODMASK XCB_MOD_MASK_4
-
 #define XHAIR_MOVE "hand1"
 #define XHAIR_SIZE "tcross"
 #define XHAIR_DFLT "left_ptr"
 
-/* window borders */
-int border = 8;
-int border_color = 0xdeadca7;
+/* window titlebars */
+int titlebar = 32;
+int titlebar_color = 0xdeadca7;
 
 /* move/resize step amound in pixels */
 int move_step = 8;
